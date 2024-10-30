10f561e5e01f38366591287342b534f3e911a97b
adm\adm <nakiri1012@gmail.com>
2024-10-30 12:23:16 +0900

    ｋｋｋｋｋ

--- CS_Form/CS_Form/Program.cs
+++ CS_Form/CS_Form/Program.cs
@@ -14,9 +14,9 @@ namespace CS_Form
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+           Application.EnableVisualStyles();
+           Application.SetCompatibleTextRenderingDefault(false);
+           Application.Run(new Form1());
         }
     }
 }
