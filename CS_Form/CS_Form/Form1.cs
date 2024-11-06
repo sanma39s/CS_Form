681c4ddf07bc6b72952bef83d45863d45bfc1fdc
adm\adm <nakiri1012@gmail.com>
2024-11-06 12:18:11 +0900

    sss

--- CS_Form/CS_Form/Form1.cs
+++ CS_Form/CS_Form/Form1.cs
@@ -12,12 +12,27 @@ namespace CS_Form
 {
     public partial class Form1 : Form
     {
+        TestLabel _TestLabel;
         public Form1()
         {
             InitializeComponent();
 
-            Testbutton testbutton = new Testbutton();
-            Controls.Add(testbutton);
+            for (int i = 0; i < 10; i++)
+            {
+                Testbutton testButton = new Testbutton(this,i,(i % 5) * 100,(i/5)*100,100,100);
+                Controls.Add(testButton);
+            }
+
+            TestLabel testLabel = new TestLabel("らべるです。",10,300,100,500);
+            Controls.Add((testLabel));
+        }
+        /// <summary>
+        /// ラベルの文字を更新する
+        /// </summary>
+        /// <param name="str"></param>
+        public void LabelTextUpdate(string str)
+        {
+            _TestLabel.TextUpdate(str);
         }
     }
 }
--- CS_Form/CS_Form/Testbutton.cs
+++ CS_Form/CS_Form/Testbutton.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +11,18 @@ namespace CS_Form
 {
     internal class Testbutton : Button
     {
-        public Testbutton()
+        Form1 _form1;
+        public Testbutton(Form1 form,int id ,int x, int y, int width,int height)
         {
+            _form1 = form;
             Click += OnClick;
+            Text = id.ToString();
+            Location = new System.Drawing.Point(x, y);
+            Size = new Size(width, height);
         }
         public void OnClick(object sender, EventArgs s)
         {
-            MessageBox.Show("おおおお");
+            MessageBox.Show(Text);
         }
     }
 
